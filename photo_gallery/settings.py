f2167a3560636656c9c525599d829f45795e3cf7
Victoria045 <victoriaberyl45@gmail.com>
2021-11-25 14:39:37 +0300

    create the application project folder

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-**!qr(mis=o&4hn&6-skf!gmnak00+ee2-6a5!z@n!06&-@szf'
+SECRET_KEY = 'django-insecure-lbhd@w!^4ewwy)u(g^_51qwjqc26ralfd)icr5^r^y^a&ng&f_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
