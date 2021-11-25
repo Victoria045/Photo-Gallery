f54f1742a86c21a21aa8209cdad03b826ad6b72f
Victoria045 <victoriaberyl45@gmail.com>
2021-11-25 15:52:06 +0300

    create the application navbar

--- posted_photos/views.py
+++ posted_photos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+# views 
+def index(request):
+     
+    return render(request,'photos/index.html')
