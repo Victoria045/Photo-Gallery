d5529b0eac76c0efe8598e052676a96c1e907fcb
Victoria045 <victoriaberyl45@gmail.com>
2021-11-28 09:06:16 +0300

    modify the application view template

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+copyLink =(element) => {
+  document.getElementById(element).select();
+  document.execCommand("copy");
+         }
+
+function scrollTo() {
+const links = document.querySelectorAll('.scroll');
+links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+e.preventDefault();
+var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+const targetAnchor = document.querySelector(targetID);
+if (!targetAnchor) return;
+const originalTop = distanceToTop(targetAnchor);
+window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+const checkIfDone = setInterval(function() {
+ const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+ if (distanceToTop(targetAnchor) === 0 || atBottom) {
+   targetAnchor.tabIndex = '-1';
+   targetAnchor.focus();
+   window.history.pushState('', '', targetID);
+   clearInterval(checkIfDone);
+ }
+}, 3000);
+}
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever')
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
