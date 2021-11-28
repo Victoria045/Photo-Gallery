e664b6947b446cc7a48c2229695dfa30caf4262b
Victoria045 <victoriaberyl45@gmail.com>
2021-11-28 09:04:35 +0300

    create the applications routes

--- posted_photos/urls.py
+++ posted_photos/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path,include
+from django.urls import path,re_path,include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     path('',views.index, name='photos-index'),
+    path('search', views.search_results, name='search'),
+    re_path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
